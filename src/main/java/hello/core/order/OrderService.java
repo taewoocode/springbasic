0edaa48fb33b9f5c6ec0c4251b13ba4aea410c17
taewoocode <qkrxodn6035@gmail.com>
2024-07-17 15:21:35 +0900

    할인정책 및 구현 만들기

--- src/main/java/hello/core/order/OrderService.java
+++ src/main/java/hello/core/order/OrderService.java
@@ -1,2 +1,6 @@
-package hello.core.order;public class OrderService {
+package hello.core.order;
+
+public interface OrderService {
+    Order createOrder(Long memberId, String itemName, int itemPirce);
+    
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,2 +1,20 @@
-package hello.core.order;public class OrderServiceImpl {
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPirce) {
+        Member member = memberRepository.findById( memberId ); //조회
+        int discountPrice = discountPolicy.discount( member, itemPirce ); //정책에 회원을 넘김
+        return new Order( memberId, itemName, itemPirce, discountPrice ); //수행된 로직들에 대한 객체 return
+    }
 }
