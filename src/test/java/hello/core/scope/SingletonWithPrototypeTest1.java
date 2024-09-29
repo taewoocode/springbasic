fb6886a4d034936c63d33f88945602f9f1d6a2d0
taewoocode <qkrxodn6035@gmail.com>
2024-09-30 02:07:33 +0900

    inject 라이브러리 추가

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,11 +3,12 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonWithPrototypeTest1 {
@@ -37,26 +38,28 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean( ClientBean.class );
         int count2 = clientBean2.logic();
-        assertThat( count2 ).isEqualTo( 2 );
+        assertThat( count2 ).isEqualTo( 1 );
 
     }
 
     @Scope("singleton") //default
-
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean; //DI
+//        private PrototypeBean prototypeBean; //DI
 
-        //@Autowired 는 생략가능
-        private ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         private int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
-            return prototypeBean.getCount();
+            int count = prototypeBean.getCount();
+            return count;
         }
     }
+//            public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+//            this.prototypeBeanProvider = prototypeBeanProvider;
+//        }
 
     @Scope("prototype")
     static class PrototypeBean {
